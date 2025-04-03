f3632f8e38c6f5db4ec77ce5da4b5f20d2c6a4be
Julian-Mirkin <49548508@est.ort.edu.ar>
2025-04-03 10:36:49 -0300

    ej 10 terminado

---
+++ Assets/Scripts/Estacionamiento.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Estacionamiento : MonoBehaviour
+{
+
+    public int hora;
+    bool abierto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(hora < 0 || hora > 24)
+        {
+            Debug.Log("Ha ingresado una hora incorrecta");
+            return;
+        } else
+        {
+            if (hora >= 10 && hora < 18)
+            {
+                abierto = true;
+            }
+            else
+            {
+                abierto = false;
+            }
+        }
+        Debug.Log("El estacionamiento está abierto: " + abierto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
