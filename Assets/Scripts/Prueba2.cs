7dba29dae1d753d3c21f871d6bccab6a4c571bde
Julian-Mirkin <49548508@est.ort.edu.ar>
2025-04-07 10:40:29 -0300

    arranque prueba 3

---
+++ Assets/Scripts/Prueba2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba2 : MonoBehaviour
+{
+    public string categoriaAnimal;
+    public int diasAQuedarse;
+
+    int gramosPorDiaGato = 300;
+    int gramosPorDiaPerroP = 400;
+    int gramosPorDiaPerroG = 700;
+
+    int gramosPorDiaAnimal;
+    float gramosNecesitados;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (categoriaAnimal == gato)
+        {
+            gramosPorDiaAnimal = gramosPorDiaGato;
+            gramosNecesitados = gramosPorDiaAnimal * diasAQuedarse;
+            Debug.Log("Para este período se necesitan " + )
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
